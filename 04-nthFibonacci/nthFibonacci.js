fc9266f55003e4576c10deaf376a5e63d62a3818
yoonah1996 <yoonah1996@gmail.com>
2020-02-21 10:25:56 +0900

    toy04

--- 04-nthFibonacci/nthFibonacci.js
+++ 04-nthFibonacci/nthFibonacci.js
@@ -21,8 +21,18 @@
  */
 
 var nthFibonacci = function (n) {
-  // TODO: implement me!
+  if(n === 0) return 0;
+  if(n === 1 || n === 2) return 1;
+  let result = 1, a = 1, b = 1;
+  for(let i=3; i<=n; i++){
+    a = b;
+    b = result;
+    result = a + b;
+  }
+  return result;
 };
+
+  // return nthFibonacci(n-1) + nthFibonacci(n-2);
 
 
 
