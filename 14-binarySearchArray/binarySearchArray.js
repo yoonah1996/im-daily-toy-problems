5da061c4f3db252ba3bc9872a863e70480385d33
yoonah1996 <yoonah1996@gmail.com>
2020-03-12 20:42:57 +0900

    삼항연산

--- 14-binarySearchArray/binarySearchArray.js
+++ 14-binarySearchArray/binarySearchArray.js
@@ -12,12 +12,29 @@ var binarySearch = function (array, target) {
     let center = array[Math.floor(array.length / 2)];
     let centerIdx = Math.floor(array.length / 2)
     
-    if(center === target){
-        return centerIdx;
-    }else if(center > target){
-        return binarySearch(array.slice(0, centerIdx), target);
-    }else if(center < target){
-        return centerIdx  + binarySearch(array.slice(centerIdx), target);
-    }
-};
+// 삼항연산으로 만들기
+return (center === target) ? 
+                centerIdx : 
+                ((center > target) ? 
+                    binarySearch(array.slice(0, centerIdx), target) :
+                    ((center < target ? 
+                        centerIdx  + binarySearch(array.slice(centerIdx), target) :
+                            null)));
 
+ 
+};
+// if(center === target){
+//     return centerIdx;
+// }else if(center > target){
+//     return binarySearch(array.slice(0, centerIdx), target);
+// }else if(center < target){
+//     return centerIdx  + binarySearch(array.slice(centerIdx), target);
+// }
+// 삼항연산으로 만들기
+// return (center === target) ? 
+//                 centerIdx : 
+//                 ((center > target) ? 
+//                     binarySearch(array.slice(0, centerIdx), target) :
+//                     ((center < target ? 
+//                         centerIdx  + binarySearch(array.slice(centerIdx), target) :
+//                             null)));
