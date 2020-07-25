53c1907e9fc428bcb3a87833c120eb4eeeac1b54
yoonah1996 <yoonah1996@gmail.com>
2020-07-25 19:33:32 +0900

    toy43 완성

--- 43-threeFiveMultiples/threeFiveMultiple.js
+++ 43-threeFiveMultiples/threeFiveMultiple.js
@@ -7,4 +7,35 @@
 
 function threeFiveMultiples(num) {
   // 여기에 코드를 작성하세요
+  // 3의 배수의 합
+  let threeRe = (threeNum) =>{
+    let totalThree = 0;
+
+    for(let i=3; i<threeNum; i = i + 3){
+      totalThree = totalThree + i;
+    }
+    return totalThree;
+  };
+
+  // 5의 배수의 합
+  let fiveRe = (fiveNum) =>{
+    let totalFive = 0;
+
+    for(let i=5; i<fiveNum; i = i + 5){
+      totalFive = totalFive + i;
+    }
+    return totalFive;
+  };
+
+  // 3의 배수와 5의 배수 중복값
+  let overlap = (num) => {
+    let totalOverlap = 0;
+
+    for(let i=15; i<num; i = i+15){
+      totalOverlap = totalOverlap + i;
+    }
+    return totalOverlap;
+  }
+
+  return threeRe(num) + fiveRe(num) - overlap(num);
 }
